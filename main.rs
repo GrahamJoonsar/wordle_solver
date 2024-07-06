43c673a922e3d979c3e1785cde9cf32f0da054d2
GrahamJoonsar <gtj162@icloud.com>
2024-07-05 19:38:41 -0700

    Added the filter for colors and fixed reading from file

--- main.rs
+++ main.rs
@@ -9,11 +9,11 @@ fn load_words(){
     let words_bytes = fs::read("words.txt").expect("Could not find \"words.txt\"");
     for i in 0..TOTAL_WORDS {
         unsafe { WORDS[i] = [
-            words_bytes[6*i+0],
-            words_bytes[6*i+1],
-            words_bytes[6*i+2],
-            words_bytes[6*i+3],
-            words_bytes[6*i+4],
+            words_bytes[7*i+0],
+            words_bytes[7*i+1],
+            words_bytes[7*i+2],
+            words_bytes[7*i+3],
+            words_bytes[7*i+4],
         ];}
     }
 }
@@ -28,6 +28,7 @@ fn word_score(word: &[u8; 5]) -> i32 {
     return score;
 }
 
+// This finds the "best word" in an array of words according to the word_score function
 fn best_word(words: Vec<[u8; 5]>) -> [u8; 5] {
     let Some(guess) = 
         words.iter()
@@ -41,10 +42,43 @@ fn best_word(words: Vec<[u8; 5]>) -> [u8; 5] {
     return *guess;
 }
 
+// Converts the ascii representation of a word to a string
 fn word_to_string(word: &[u8; 5]) -> String {
     return String::from(std::str::from_utf8(word).expect("invalid utf-8 sequence"));
 }
 
+// Represents the constraints placed on the word, (character, position)
+enum Filter {
+    Green(u8, usize),
+    Yellow(u8, usize),
+    Black(u8),
+}
+
+fn filter_words(mut words: Vec<[u8; 5]>, filters: &Vec<Filter>) -> Vec<[u8; 5]>{
+    for filter in filters {
+        match filter {
+            Filter::Green(letter, pos) => {
+                words = words.into_iter()
+                    .filter(|w| w[*pos] == *letter)
+                    .collect();
+            },
+            Filter::Yellow(letter, pos) => {
+                words = words.into_iter()
+                    .filter(|w| w.contains(letter) && w[*pos] != *letter)
+                    .collect();
+            },
+            Filter::Black(letter) => {
+                words = words.into_iter()
+                    .filter(|w| !w.contains(letter))
+                    .collect();
+            },
+        }
+    }
+    return words;
+}
+
 fn main() {
     load_words();
+    let vec_words: Vec<[u8; 5]> = unsafe { WORDS.to_vec() };
+    println!("{:?}", filter_words(vec_words, &vec![Filter::Black(97), Filter::Black(97 + 4), Filter::Black(99), Filter::Green(97 + 25, 0)]));
 }
